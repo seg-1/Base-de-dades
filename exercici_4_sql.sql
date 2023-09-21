0aa9566ed11da1df6711420304bc2402ee9c01d8
seg-1 <sergiexgon@gmail.com>
2023-09-21 16:43:27 +0200

    Añadidas soluciones y actualizados ejercicios 3 y 4
    Añadidas las capturas de pantalla con las soluciones.
    Actualizados los ejercicios 3 y 4 para tener la query en dos líneas más cortas.

--- exercici_4_sql.sql
+++ exercici_4_sql.sql
@@ -3,4 +3,5 @@
 -- Has d'obtenir, per a cada persona, el seu nom i el nombre màxim de rols diferents que ha tingut en una mateixa pel·lícula. 
 -- Posteriorment, mostra únicament aquelles persones que hagin assumit més d'un rol en una mateixa pel·lícula.
 
-SELECT (SELECT DISTINCT person_name FROM tb_person AS pe WHERE pe.person_id = mp.person_id) AS person_name , COUNT(role_id) AS num_rol_movie FROM tb_movie_person AS mp GROUP BY person_id, movie_id HAVING num_rol_movie > 1;
+SELECT (SELECT DISTINCT person_name FROM tb_person AS pe WHERE pe.person_id = mp.person_id) AS person_name , COUNT(role_id) AS num_rol_movie FROM tb_movie_person AS mp 
+GROUP BY person_id, movie_id HAVING num_rol_movie > 1;
